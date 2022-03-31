5168f765412e33e7fe2cf76f6f3bc8c01871381d
Gian Clarke <gianclarke@hotmail.com>
2022-04-01 11:08:14 +1300

    Added loading for mobile view

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+if ($(window).width() < 771) {
+  document.getElementById("loading").style.display = "flex";
+  const wait = (delay = 0) =>
+  new Promise(resolve => setTimeout(resolve, delay));
+
+  const setVisible = (elementOrSelector, visible) => 
+    (typeof elementOrSelector === 'string'
+      ? document.querySelector(elementOrSelector)
+      : elementOrSelector
+    ).style.display = visible ? 'block' : 'none';
+
+  setVisible('.page', false);
+  setVisible('#loading', true);
+
+  document.addEventListener('DOMContentLoaded', () =>
+    wait(2000).then(() => {
+      setVisible('.page', true);
+      setVisible('#loading', false);
+  }));
+}
+
+
+
+
+
+
 const navLink = document.querySelectorAll('.navigation__link');
 const sections = document.querySelectorAll("section");
 window.addEventListener("scroll", () => {
